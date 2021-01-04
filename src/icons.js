0e88cd436934f200239bdef7f1c0e4cb4a30e243
Sirawit Pratoomsuwan <s36526@bj.ac.th>
2021-01-04 13:20:04 +0700

    Init

---
+++ src/icons.js
@@ -0,0 +1,29 @@
+module.exports = {
+    ["nuxt.config.js"]: {
+        icon: "nuxt",
+        text: "Nuxt.js"
+    },
+    js: {
+        icon: "js",
+        text: "Javascript"
+    },
+    ["yarn.lock"]: {
+        icon: "yarn",
+        text: "Yarn"
+    },
+    ["package.json"]: {
+        icon: "npm",
+        text: "Node Package Manager"
+    },
+    json: {
+        icon: "json",
+        text: "JSON"
+    },
+    html: {
+        icon: "html",
+        text: "HyperText Markup Language"
+    },
+    css: {
+
+    }
+}
