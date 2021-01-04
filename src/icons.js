047bc09a9aee2abf08eab589f988833b7d35eedc
Thun (Swift3 Windows) <bsthun@gmail.com>
2021-01-04 18:07:11 +0700

    Contribute on icons

--- src/icons.js
+++ src/icons.js
@@ -1,29 +1,70 @@
 module.exports = {
-    ["nuxt.config.js"]: {
-        icon: "nuxt",
-        text: "Nuxt.js"
-    },
-    js: {
-        icon: "js",
-        text: "Javascript"
-    },
-    ["yarn.lock"]: {
-        icon: "yarn",
-        text: "Yarn"
-    },
-    ["package.json"]: {
-        icon: "npm",
-        text: "Node Package Manager"
-    },
-    json: {
-        icon: "json",
-        text: "JSON"
-    },
-    html: {
-        icon: "html",
-        text: "HyperText Markup Language"
-    },
-    css: {
-
-    }
+	["nuxt.config.js"]: {
+		icon: "nuxt",
+		text: "Nuxt.js"
+	},
+	["yarn.lock"]: {
+		icon: "yarn",
+		text: "Yarn"
+	},
+	["package.json"]: {
+		icon: "npm",
+		text: "Node Package Manager"
+	},
+	["docker-compose.yml"]: {
+		icon: "npm",
+		text: "Node Package Manager"
+	},
+	["firebase.yml"]: {
+		icon: "npm",
+		text: "Node Package Manager"
+	},
+	dart: {
+		icon: "dart",
+		text: "Dart",
+	},
+	go: {
+		icon: "go",
+		text: "Golang"
+	},
+	html: {
+		icon: "html",
+		text: "HyperText Markup Language"
+	},
+	java: {
+		icon: "java",
+		text: "Java"
+	},
+	js: {
+		icon: "js",
+		text: "Javascript"
+	},
+	json: {
+		icon: "json",
+		text: "JSON"
+	},
+	key: {
+		icon: "key",
+		text: "Private Key"
+	},
+	less: {
+		icon: "less",
+		text: "Less"
+	},
+	lock: {
+		icon: "lock",
+		text: "Lock"
+	},
+	log: {
+		icon: "log",
+		text: "Log"
+	},
+	lua: {
+		icon: "lua",
+		text: "Lua"
+	},
+	md: {
+		icon: "markdown",
+		text: "Markdown"
+	}
 }
--- src/main.js
+++ src/main.js
@@ -1,66 +1,64 @@
 const RPC = require('discord-rpc');
-let icons = require('./test_icon');
+let icons = require('./icons');
 var monitor = require('active-window');
 
 let iconPacks = icons;
 
 const rpc = new RPC.Client({
-    transport: "ipc"
+	transport: "ipc"
 });
 
 rpc.on('ready', () => {
-    monitor.getActiveWindow(callback, -1, 1);
-    console.log("Rich is now active!");
+	monitor.getActiveWindow(callback, -1, 1);
+	console.log("Rich is now active!");
 });
-var startTimestamp = new Date()
-function onWindowChange({ app, title }) {
-    console.log("Window changed!");
-    var details = "Hey, I ❤️ coding with..";
-    var state = "Node.js x Vue.js";
-    if (app === "Code - Insiders") {
 
-        state = `Editing ${title.split(" - ")[0].trim()} in ${title.split(" - ")[1].trim()}.`
+var startTimestamp = new Date();
 
+const onWindowChange = ({ app, title }) => {
+	console.log("Window changed!");
+	var details = "Hey, I ❤️ coding with..";
+	var state = "Node.js x Vue.js";
+	if (app === "Code - Insiders") {
+		state = `Editing ${title.split(" - ")[0].trim()} in ${title.split(" - ")[1].trim()}.`
 
-        const fileName = title.split(" - ")[0].trim();
-        const { icon, text } = checkIfMatched(fileName);
-        console.log(`show -> ${text}`);
-        rpc.setActivity({
-            details,
-            state,
-            startTimestamp,
-            smallImageText: "Visual Studio Code",
-            smallImageKey: "vscode",
-            largeImageText: text,
-            largeImageKey: icon
-        });
-    }
+		const fileName = title.split(" - ")[0].trim();
+		const { icon, text } = checkIfMatched(fileName);
+		console.log(`show -> ${text}`);
+		rpc.setActivity({
+			details,
+			state,
+			startTimestamp,
+			smallImageText: "Visual Studio Code",
+			smallImageKey: "vscode",
+			largeImageText: text,
+			largeImageKey: icon
+		});
+	}
 }
 
 function checkIfMatched(fileName) {
-    for (var icon in iconPacks) {
-        if (fileName.endsWith(icon)) {
-            return iconPacks[icon];
-        }
-    }
-    return { icon: "nodejslogo", text: "Node.js" };
+	for (var icon in iconPacks) {
+		if (fileName.endsWith(icon)) {
+			return iconPacks[icon];
+		}
+	}
+	return { icon: "nodejslogo", text: "Node.js" };
 }
 
 rpc.login({
-    clientId: "795279750506741801",
+	clientId: "795279750506741801",
 });
-
 
 let actualTitle = "";
 
-callback = function (window) {
-    if (actualTitle !== window.title) {
-        onWindowChange(window);
-    }
-    try {
-        actualTitle = window.title;
-    } catch (err) {
-        console.log(err);
-    }
-
+callback = (window) => {
+	if (actualTitle !== window.title) {
+		onWindowChange(window);
+	}
+	try {
+		actualTitle = window.title;
+	} catch (err) {
+		console.log(err);
+	}
 }
